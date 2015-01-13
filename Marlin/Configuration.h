301b4b246f34c6be496f34f946819b80fa943f8c
Michael <wyrick@umbrasi.com>
2015-01-12 21:33:58 -0500

    Set max movement to measured movement amounts.   The Y is limited by the Clips used to hold the glass down.   X is limited by glass size.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -370,9 +370,9 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
 
 // Travel limits after homing
-#define X_MAX_POS 200
+#define X_MAX_POS 194
 #define X_MIN_POS 0
-#define Y_MAX_POS 200
+#define Y_MAX_POS 188
 #define Y_MIN_POS 0
 #define Z_MAX_POS 190
 #define Z_MIN_POS 0
@@ -443,7 +443,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
   #define Y_PROBE_OFFSET_FROM_EXTRUDER -12 
   #define Z_PROBE_OFFSET_FROM_EXTRUDER -3.7
 
-  #define Z_RAISE_BEFORE_HOMING 10       // (in mm) Raise Z before homing (G28) for Probe Clearance.
+  #define Z_RAISE_BEFORE_HOMING 5        // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                          // Be sure you have this distance over your Z_MAX_POS in case
 
   #define XY_TRAVEL_SPEED 4000         // X and Y axis travel speed between probes, in mm/min
