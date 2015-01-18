ff720455187962651d12d12d4a5f9054485108bf
Michael <wyrick@umbrasi.com>
2015-01-18 13:13:19 -0500

    Added setting of the feedrate to the XY_TRAVEL_SPEED to get the move to a sane speed.

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -1227,7 +1227,8 @@ void moveafterhome() {
   #ifdef Z_MOVEAFTERHOME 
   destination[Z_AXIS] = Z_MOVEAFTERHOME;
   #endif
-  plan_buffer_line(destination[X_AXIS], destination[Y_AXIS], destination[Z_AXIS], destination[E_AXIS], feedrate/60, active_extruder);
+  feedrate = XY_TRAVEL_SPEED;
+  plan_buffer_line(destination[X_AXIS], destination[Y_AXIS], destination[Z_AXIS], destination[E_AXIS], feedrate, active_extruder);
   st_synchronize();
   current_position[X_AXIS] = destination[X_AXIS];
   current_position[Y_AXIS] = destination[Y_AXIS];
